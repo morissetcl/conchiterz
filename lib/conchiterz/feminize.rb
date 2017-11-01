b8c127b0572d9be0f1a431b1b7f167ee0e88b532
Clement Morisset <morissetcl87@gmail.com>
2017-11-01 19:41:16 +0100

    refactor code

--- lib/conchiterz/feminize.rb
+++ lib/conchiterz/feminize.rb
@@ -5,39 +5,49 @@ module Conchiterz
       result = []
       a_words = string.split
       a_words.each do |word|
-        if TRANSLATION.has_value?(word.downcase)
-          a = TRANSLATION.key(word.downcase)
-          capitalized = (word == word.capitalize)
-          upcased = (word == word.upcase)
-          if check_sensitive(capitalized, upcased) == false
-            result << a
-          else
-            result << a.capitalize if capitalized
-            result << a.upcase if upcased
-          end
-        elsif TRANSLATION.has_key?(word.downcase)
-          b = TRANSLATION.fetch(word.downcase)
-          capitalized = (word == word.capitalize)
-          upcased = (word == word.upcase)
-          if check_sensitive(capitalized, upcased) == false
-            result << b
-          else
-            result << b.capitalize if capitalized
-            result << b.upcase if upcased
-          end
-        else
-          result.push(word)
-        end
+        analyze_string(word, result)
       end
       result.join(' ')
     end
 
     private
-    
-    def self.check_sensitive(capitalized, upcased)
-      if (capitalized == false && upcased == false)
-        false
+
+    def self.analyze_string(word, result)
+      capitalized = (word == word.capitalize)
+      upcased = (word == word.upcase)
+      if TRANSLATION.has_value?(word.downcase)
+        get_key(capitalized, upcased, result, word)
+      elsif TRANSLATION.has_key?(word.downcase)
+        get_value(capitalized, upcased, result, word)
+      else
+        result.push(word)
       end
+    end
+
+    def self.get_key(capitalized, upcased, result, word)
+      key = TRANSLATION.key(word.downcase)
+      if check_sensitive(capitalized, upcased) == false
+        result << key
+      else
+        result << key.capitalize if capitalized
+        result << key.upcase if upcased
+      end
+    end
+
+
+    def self.get_value(capitalized, upcased, result, word)
+      value = TRANSLATION.fetch(word.downcase)
+      if check_sensitive(capitalized, upcased) == false
+        result << value
+      else
+        result << value.capitalize if capitalized
+        result << value.upcase if upcased
+      end
+    end
+
+    def self.check_sensitive(capitalized, upcased)
+      return unless (capitalized == false && upcased == false)
+      false
     end
 
     TRANSLATION =
