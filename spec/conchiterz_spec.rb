982a5588206a3c31b13fa046fd574a38ea49e61d
Clement Morisset <morissetcl87@gmail.com>
2017-11-01 17:51:59 +0100

    become case insensitive, at east for capitalize

--- spec/conchiterz_spec.rb
+++ spec/conchiterz_spec.rb
@@ -2,9 +2,12 @@ require 'conchiterz'
 
 describe Conchiterz::Feminize do
   it "feminize a word" do
-    expect(Conchiterz::Feminize.translate("beau et cool")).to eql("belle et cool")
+    expect(Conchiterz::Feminize.translate("il est beau et joueur")).to eql("elle est belle et joueuse")
   end
   it "masculanize a word" do
-    expect(Conchiterz::Feminize.translate("belle et cool")).to eql("beau et cool")
+    expect(Conchiterz::Feminize.translate("elle est belle et joueuse")).to eql("il est beau et joueur")
+  end
+  it "insensitive", :focus do
+    expect(Conchiterz::Feminize.translate("Elle est Belle et Joueuse")).to eql("Il est Beau et Joueur")
   end
 end
